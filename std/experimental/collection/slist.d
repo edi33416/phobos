e459f20be098bb9b55564129968515d5b10894ab
Eduard Staniloiu <edi33416@gmail.com>
2017-02-23 16:06:28 +0200

    Add partial fix

--- std/experimental/collection/slist.d
+++ std/experimental/collection/slist.d
@@ -30,6 +30,7 @@ private:
     }
 
     Node *_head;
+    Node *_sentinel;
 
     alias Alloc = AffixAllocator!(GCAllocator, uint);
     alias _allocator = Alloc.instance;
@@ -50,6 +51,8 @@ private:
     @trusted void delRef(Node *node)
     {
         assert(node !is null);
+        import std.stdio;
+        writefln("In delRef bef prefCount Node %s\n", node._payload);
         uint *pref = prefCount(node);
         debug(CollectionSList) writefln("In delRef for node %s. Has refcount: %s; will be: %s",
                 node._payload, *pref, *pref - 1);
@@ -81,6 +84,7 @@ public:
     if (isImplicitlyConvertible!(U, T))
     {
         //_allocator = allocator;
+        _head = _sentinel = _allocator.make!(Node)(0, null);
         insert(values);
     }
 
@@ -98,11 +102,17 @@ public:
         && !is(Stuff == T[]))
     {
         //_allocator = allocator;
+        _head = _sentinel = _allocator.make!(Node)(0, null);
         insert(stuff);
     }
 
     this(this)
     {
+        if (_sentinel is null)
+        {
+            _head = _sentinel = _allocator.make!(Node)(0, null);
+        }
+
         uint *pref = prefCount(_head);
         addRef(_head);
         debug(CollectionSList) writefln("In postblit for node %s. Has refcount: %s",
@@ -111,6 +121,7 @@ public:
 
     ~this()
     {
+        writefln("Begin Dtor");
         while (_head !is null && *prefCount(_head) == 0)
         {
             debug(CollectionSList) writefln("In dtor once. Head at %s", _head._payload);
@@ -127,11 +138,14 @@ public:
             debug(CollectionSList) writefln("In dtor twice. Head at %s", _head._payload);
             delRef(_head);
         }
+        writefln("End Dtor");
     }
+
+    typeof(this) opAssign();
 
     bool empty()
     {
-        return _head is null;
+        return _head is _sentinel;
     }
 
     T front()
@@ -143,12 +157,22 @@ public:
     void popFront()
     {
         assert(!empty, "SList.popFront: List is empty");
+
+        import std.stdio;
+        writefln("In pop front for node %s", _head._payload);
+
         Node *tmpNode = _head;
         _head = _head._next;
-        if (_head !is null)
+        writeln("PF bef if");
+        if (*prefCount(tmpNode) > 0 &&  _head !is null)
         {
+            // If we have another copy of the list then the refcount
+            // must increase, otherwise it will remain the same
+            writeln("PF bef add ref");
             addRef(_head);
         }
+
+        writeln("PF bef del ref");
         delRef(tmpNode);
     }
 
@@ -161,6 +185,12 @@ public:
     if (isInputRange!Stuff && isImplicitlyConvertible!(ElementType!Stuff, T))
     {
         //if (_allocator is null) _allocator = theAllocator;
+        writeln("Here");
+        if (_sentinel is null)
+        {
+            _head = _sentinel = _allocator.make!(Node)(0, null);
+        }
+
 
         size_t result;
         Node *tmpNode;
@@ -196,13 +226,8 @@ public:
 
     void remove()
     {
-        Node *tmpNode = _head;
-        _head = _head._next;
-        delRef(tmpNode);
-        if (_head !is null)
-        {
-            addRef(_head);
-        }
+        assert(!empty, "SList.remove: List is empty");
+        popFront();
     }
 
     debug(CollectionSList) void printRefCount()
@@ -219,78 +244,145 @@ public:
 
 }
 
+@trusted unittest
+{
+    auto sl = SList!int(1);
+    //auto sl2 = sl;
+
+    debug(CollectionSList) sl.printRefCount();
+    //debug(CollectionSList) sl2.printRefCount();
+}
+
+//@trusted unittest
+//{
+    //import std.algorithm.comparison : equal;
+    //import std.algorithm.searching : canFind, find;
+
+    //auto sl = SList!int();
+    //assert(sl.empty);
+
+    //sl.insert(1);
+    //sl.insert([2]);
+    //sl.insert([3, 4, 5]);
+    //sl.insert(6, 7, 8);
+
+    //assert(equal(sl, [6, 7, 8, 3, 4, 5, 2, 1]));
+
+    //sl.remove();
+    //assert(equal(sl, [7, 8, 3, 4, 5, 2, 1]));
+
+    //assert(canFind(sl, 2));
+    //auto sl2 = sl;
+
+    //import std.stdio;
+    //writefln("Can find -1? A: %s\n", sl.canFind(-1));
+//}
+
+//@trusted unittest
+//{
+    //import std.algorithm.comparison : equal;
+
+    //auto sl = SList!int();
+    //sl.insert(10, 20, 30);
+
+    //auto sl2 = SList!int(sl);
+
+    //assert(equal(sl, sl2));
+    //writefln("Are equal? A: %s\n", equal(sl, sl2));
+    //debug(CollectionSList)
+    //{
+        //writefln("For sl");
+        //sl.printRefCount();
+        //writefln("\nFor sl2");
+        //sl2.printRefCount();
+    //}
+
+    //sl.popFront();
+    //assert(equal(sl, [20, 30]));
+    //assert(equal(sl2, [10, 20, 30]));
+    //debug(CollectionSList)
+    //{
+        //writefln("For sl");
+        //sl.printRefCount();
+        //writefln("\nFor sl2");
+        //sl2.printRefCount();
+    //}
+
+    //writeln("End of unittest");
+//}
+
 void main(string[] args)
 {
-    debug(CollectionSList) writefln("Begining of main\n");
+    //debug(CollectionSList) writefln("Begining of main\n");
 
-    auto sl = SList!(int)();
-    sl.insert(10);
-    //if (sl !is null)
-        //writeln(sl.empty());
-    debug(CollectionSList)
-    {
-        writeln("After insert");
-        sl.printRefCount();
-    }
+    //auto sl = SList!(int)();
+    //sl.insert(10);
+    ////if (sl !is null)
+        ////writeln(sl.empty());
+    //debug(CollectionSList)
+    //{
+        //writeln("After insert");
+        //sl.printRefCount();
+    //}
 
-    sl.insert(11, 21, 31, 41, 51);
-    debug(CollectionSList)
-    {
-        writeln("After insert");
-        sl.printRefCount();
-    }
+    //sl.insert(11, 21, 31, 41, 51);
+    //debug(CollectionSList)
+    //{
+        //writeln("After insert");
+        //sl.printRefCount();
+    //}
 
-    sl.insert([12, 22, 32, 42, 52]);
-    debug(CollectionSList)
-    {
-        writeln("After insert");
-        sl.printRefCount();
-    }
+    //sl.insert([12, 22, 32, 42, 52]);
+    //debug(CollectionSList)
+    //{
+        //writeln("After insert");
+        //sl.printRefCount();
+    //}
 
-    int i;
-    auto sl2 = sl;
-    //auto sl3 = sl;
-    debug(CollectionSList)
-    {
-        writeln("After sl2");
-        sl.printRefCount();
-    }
+    //int i;
+    //auto sl2 = sl;
+    ////auto sl3 = sl;
+    //debug(CollectionSList)
+    //{
+        //writeln("After sl2");
+        //sl.printRefCount();
+    //}
 
-    while (!sl.empty())
-    {
-        debug(CollectionSList) writefln("Elem %s: %s", ++i, sl.front);
-        sl.popFront;
-        debug(CollectionSList) sl.printRefCount();
-    }
+    //while (!sl.empty())
+    //{
+        //writefln("Elem %s: %s", ++i, sl.front);
+        //sl.popFront;
+        //debug(CollectionSList) sl.printRefCount();
+    //}
 
-    debug(CollectionSList)
-    {
-        writeln();
-        sl2.printRefCount();
-    }
+    //debug(CollectionSList)
+    //{
+        //writeln();
+        //sl2.printRefCount();
+    //}
 
-    auto sl3 = sl2;
-    int needle = 10;
-    debug(CollectionSList)
-    {
-        writeln("\nBefore find");
-        writefln("Can find %s in list? A: %s\n", needle, canFind(sl3, needle));
-        writeln("After find\n");
-    }
-    else
-    {
-        assert(canFind(sl3, needle));
-    }
+    //auto sl3 = sl2;
+    //int needle = 10;
+    //debug(CollectionSList)
+    //{
+        //writeln("\nBefore find");
+        //writefln("Can find %s in list? A: %s\n", needle, canFind(sl3, needle));
+        //writeln("After find\n");
+    //}
+    //else
+    //{
+        //assert(canFind(sl3, needle));
+    //}
 
-    debug(CollectionSList) writefln("Removing Node with value: %s\n", sl2.front);
-    //sl2.remove();
-    sl2.popFront();
-    i = 0;
-    while (!sl2.empty())
-    {
-        debug(CollectionSList) writefln("Elem %s: %s", ++i, sl2.front);
-        sl2.popFront;
-    }
+    //debug(CollectionSList) writefln("Removing Node with value: %s\n", sl2.front);
+    ////sl2.remove();
+    //sl2.popFront();
+    //i = 0;
+    //while (!sl2.empty())
+    //{
+        //debug(CollectionSList) writefln("Elem %s: %s", ++i, sl2.front);
+        //sl2.popFront;
+    //}
 
-    debug(CollectionSList) writefln("\nLeaving main");
+    //debug(CollectionSList) writefln("\nLeaving main");
 }
