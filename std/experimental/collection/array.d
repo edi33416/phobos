8759b58af142df3b60db81cd710fc6c645a0cdd3
Eduard Staniloiu <edi33416@gmail.com>
2017-04-21 00:16:44 +0300

    [Array] add op overloads, opSlice, opAssign and tail

--- std/experimental/collection/array.d
+++ std/experimental/collection/array.d
@@ -21,8 +21,9 @@ version(unittest)
 
 struct Array(T)
 {
-    import std.traits : isImplicitlyConvertible, Unqual;
-    import std.range.primitives : isInputRange, isForwardRange, ElementType;
+    import std.traits : isImplicitlyConvertible, Unqual, isArray;
+    import std.range.primitives : isInputRange, isForwardRange, isInfinite,
+           ElementType, hasLength;
     import std.conv : emplace;
     import core.atomic : atomicOp;
 
@@ -30,6 +31,7 @@ struct Array(T)
     Unqual!T[] _support;
 
     static enum double capacityFactor = 3.0 / 2;
+    static enum initCapacity = 3;
 
     version(unittest) { } else
     {
@@ -37,7 +39,7 @@ struct Array(T)
         Alloc _allocator;
     }
 
-    @trusted void addRef(this Qualified)(Unqual!T[] support)
+    @trusted void addRef(SupportQual, this Qualified)(SupportQual support)
     {
         assert(support !is null);
         debug(CollectionArray)
@@ -72,7 +74,7 @@ struct Array(T)
         }
     }
 
-    @trusted auto prefCount(this Qualified)(Unqual!T[] support)
+    @trusted auto prefCount(SupportQual, this Qualified)(SupportQual support)
     {
         assert(support !is null);
         version(unittest)
@@ -92,15 +94,35 @@ struct Array(T)
         }
     }
 
-    static string immutableInsert(string stuff)
+    static string immutableInsert(StuffType)(string stuff)
     {
+        static if (hasLength!StuffType)
+        {
+            auto stuffLengthStr = ""
+                ~"{"
+                    ~"stuffLength = " ~ stuff ~ ".length;"
+                ~"}";
+        }
+        else
+        {
+            auto stuffLengthStr = ""
+                ~"{"
+                    ~"import std.range.primitives : walkLength;"
+                    ~"stuffLength = walkLength(" ~ stuff ~ ");"
+                ~"}";
+        }
+
         return ""
-            ~"if (stuff.length > slackBack)"
-            ~"{"
-                ~"reserve(capacity + stuff.length);"
-            ~"}"
-            ~"_support[_payload.length .. _payload.length + stuff.length] = stuff[];"
-            ~"_payload = cast(Unqual!T[])(_support[0 .. _payload.length + stuff.length]);";
+        ~"size_t stuffLength = 0;"
+        ~ stuffLengthStr
+        ~"auto tmpSupport = cast(Unqual!T[])(_allocator.allocate(stuffLength * T.sizeof));"
+        ~"size_t i = 0;"
+        ~"foreach (item; " ~ stuff ~ ")"
+        ~"{"
+            ~"tmpSupport[i++] = item;"
+        ~"}"
+        ~"_support = cast(typeof(_support))(tmpSupport);"
+        ~"_payload = cast(T[])(_support[0 .. stuffLength]);";
     }
 
     void destroyUnused()
@@ -137,7 +159,7 @@ public:
         }
         static if (is(Qualified == immutable) || is(Qualified == const))
         {
-            mixin(immutableInsert("values"));
+            mixin(immutableInsert!(typeof(values))("values"));
         }
         else
         {
@@ -146,7 +168,7 @@ public:
     }
 
     this(Stuff, this Qualified)(Stuff stuff)
-    if (isInputRange!Stuff
+    if (isInputRange!Stuff && !isInfinite!Stuff
         && isImplicitlyConvertible!(ElementType!Stuff, T)
         && !is(Stuff == T[]))
     {
@@ -154,7 +176,7 @@ public:
     }
 
     this(Stuff, this Qualified)(IAllocator allocator, Stuff stuff)
-    if (isInputRange!Stuff
+    if (isInputRange!Stuff && !isInfinite!Stuff
         && isImplicitlyConvertible!(ElementType!Stuff, T)
         && !is(Stuff == T[]))
     {
@@ -169,7 +191,7 @@ public:
         }
         static if (is(Qualified == immutable) || is(Qualified == const))
         {
-            mixin(immutableInsert("stuff"));
+            mixin(immutableInsert!(typeof(stuff))("stuff"));
         }
         else
         {
@@ -191,21 +213,6 @@ public:
                     _support, *prefCount)();
         }
     }
-
-    // Immutable ctors
-    //private this(NodeQual, this Qualified)(NodeQual _newHead)
-        //if (is(typeof(_head) : typeof(_newHead))
-            //&& (is(Qualified == immutable) || is(Qualified == const)))
-    //{
-        //_head = _newHead;
-        //if (_head !is null)
-        //{
-            //shared uint *pref = prefCount(_head);
-            //addRef(_head);
-            //debug(CollectionArray) writefln("Array.ctor immutable: Node %s has "
-                    //~ "refcount: %s", _head._payload, *pref);
-        //}
-    //}
 
     @trusted ~this()
     {
@@ -233,6 +240,8 @@ public:
     {
         return _payload.length;
     }
+
+    alias opDollar = length;
 
     @trusted size_t capacity() const
     {
@@ -271,29 +280,44 @@ public:
         assert(capacity >= n);
     }
 
-    //size_t insert(Stuff)(size_t pos, Stuff stuff)
-    //if (isInputRange!Stuff && isImplicitlyConvertible!(ElementType!Stuff, T))
-    //{
-        //debug(CollectionArray)
-        //{
-            //writefln("Array.insert: begin");
-            //scope(exit) writefln("Array.insert: end");
-        //}
-        //version(unittest) { } else
-        //{
-            //if (() @trusted { return _allocator.parent is null; }())
-            //{
-                //_allocator = AffixAllocator!(IAllocator, size_t)(theAllocator);
-            //}
-        //}
+    size_t insert(Stuff)(size_t pos, Stuff stuff)
+    if (!isArray!(typeof(stuff)) && isInputRange!Stuff && !isInfinite!Stuff
+        && isImplicitlyConvertible!(ElementType!Stuff, T))
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.insert: begin");
+            scope(exit) writefln("Array.insert: end");
+        }
+        version(unittest) { } else
+        {
+            if (() @trusted { return _allocator.parent is null; }())
+            {
+                _allocator = AffixAllocator!(IAllocator, size_t)(theAllocator);
+            }
+        }
 
-        //size_t result;
-        //foreach (item; stuff)
-        //{
-            //++result;
-        //}
-        //return result;
-    //}
+        size_t stuffLength = 0;
+        static if (hasLength!Stuff)
+        {
+            stuffLength = stuff.length;
+        }
+        else
+        {
+            import std.range.primitives : walkLength;
+            stuffLength = walkLength(stuff);
+        }
+
+        auto tmpSupport = cast(Unqual!T[])(_allocator.allocate(stuffLength * T.sizeof));
+        size_t i = 0;
+        foreach (item; stuff)
+        {
+            tmpSupport[i++] = item;
+        }
+        size_t result = insert(pos, tmpSupport);
+        _allocator.dispose(tmpSupport);
+        return result;
+    }
 
     size_t insert(Stuff)(size_t pos, Stuff[] stuff...)
     if (isImplicitlyConvertible!(Stuff, T))
@@ -350,6 +374,160 @@ public:
         assert(!empty, "Array.popFront: Array is empty");
         _payload = _payload[1 .. $];
     }
+
+    Qualified tail(this Qualified)()
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.tail: begin");
+            scope(exit) writefln("Array.tail: end");
+        }
+        assert(!empty, "Array.tail: Array is empty");
+
+        static if (is(Qualified == immutable) || is(Qualified == const))
+        {
+            return this[1 .. $];
+        }
+        else
+        {
+            return .tail(this);
+        }
+    }
+
+    ref auto save(this _)()
+    {
+        debug(CollectionSList)
+        {
+            writefln("Array.save: begin");
+            scope(exit) writefln("Array.save: end");
+        }
+        return this;
+    }
+
+    typeof(this) dup()
+    {
+        debug(CollectionSList)
+        {
+            writefln("Array.dup: begin");
+            scope(exit) writefln("Array.dup: end");
+        }
+        return typeof(this)(this);
+    }
+
+    Qualified opSlice(this Qualified)()
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.opSlice(): begin");
+            scope(exit) writefln("Array.opSlice(): end");
+        }
+        return this.save;
+    }
+
+    Qualified opSlice(this Qualified)(size_t start, size_t end)
+    in
+    {
+        assert(start <= end && end <= length,
+               "Array.opSlice(s, e): Invalid bounds: Ensure start <= end <= length");
+    }
+    body
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.opSlice(s, e): begin");
+            scope(exit) writefln("Array.opSlice(s, e): end");
+        }
+        Unqual!(typeof(this)) result;
+        result._support = cast(typeof(result._support))(_support);
+        result._payload = cast(typeof(result._payload))(_payload[start .. end]);
+        addRef(_support);
+        return cast(typeof(this))(result);
+    }
+
+    ref auto opIndex(this _)(size_t idx)
+    in
+    {
+        assert(idx <= length, "Array.opIndex: Index out of bounds");
+    }
+    body
+    {
+        return _payload[idx];
+    }
+
+    ref auto opIndexUnary(string op)(size_t idx)
+    in
+    {
+        assert(idx <= length, "Array.opIndexUnary!" ~ op ~ ": Index out of bounds");
+    }
+    body
+    {
+        mixin("return " ~ op ~ "_payload[idx];");
+    }
+
+    ref auto opIndexAssign(U)(U elem, size_t idx)
+    if (isImplicitlyConvertible!(U, T))
+    in
+    {
+        assert(idx <= length, "Array.opIndexAssign: Index out of bounds");
+    }
+    body
+    {
+        _payload[idx] = elem;
+    }
+
+    ref auto opIndexOpAssign(string op, U)(U elem, size_t idx)
+    if (isImplicitlyConvertible!(U, T))
+    in
+    {
+        assert(idx <= length, "Array.opIndexOpAssign!" ~ op ~ ": Index out of bounds");
+    }
+    body
+    {
+        mixin("return _payload[idx]" ~ op ~ "= elem;");
+    }
+
+    auto ref opAssign()(auto ref typeof(this) rhs)
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.opAssign: begin");
+            scope(exit) writefln("Array.opAssign: end");
+        }
+
+        if (rhs._support !is null && _support is rhs._support)
+        {
+            return this;
+        }
+
+        if (rhs._support !is null)
+        {
+            addRef(rhs._support);
+            debug(CollectionArray) writefln("Array.opAssign: Array %s has refcount: %s",
+                    rhs._payload, *prefCount(rhs._support));
+        }
+        __dtor();
+        _support = rhs._support;
+        _payload = rhs._payload;
+
+        return this;
+    }
+
+    auto ref opOpAssign(string op, U)(auto ref U rhs)
+        if (op == "~" &&
+            (is (U == typeof(this))
+             || is (U : T)
+             || (isInputRange!U && isImplicitlyConvertible!(ElementType!U, T))
+            ))
+    {
+        debug(CollectionArray)
+        {
+            writefln("Array.opOpAssign!~: %s begin", typeof(this).stringof);
+            scope(exit) writefln("Array.opOpAssign!~: %s end", typeof(this).stringof);
+        }
+        insert(length, rhs);
+        return this;
+    }
+
 }
 
 version(unittest) private @trusted void testInit()
@@ -359,6 +537,7 @@ version(unittest) private @trusted void testInit()
     writefln("Array %s cap %s", v, v.capacity);
     v.insert(1, 1, 2);
     writefln("Array %s cap %s", v, v.capacity);
+    auto t = v.dup();
 }
 
 @safe unittest
@@ -367,6 +546,46 @@ version(unittest) private @trusted void testInit()
     testInit();
     auto bytesUsed = _allocator.bytesUsed;
     assert(bytesUsed == 0, "Array ref count leaks memory; leaked "
+                           ~ to!string(bytesUsed) ~ " bytes");
+}
+
+version(unittest) private @trusted void testImmutability()
+{
+    auto a = immutable Array!(int)(1, 2, 3);
+    auto a2 = a;
+    auto a3 = a2.save();
+
+    assert(a2.front == 1);
+    static assert(!__traits(compiles, a2.front = 4));
+    static assert(!__traits(compiles, a2.popFront()));
+
+    auto a4 = a2.tail;
+    assert(a4.front == 2);
+    static assert(!__traits(compiles, a4 = a4.tail));
+}
+
+version(unittest) private @trusted void testConstness()
+{
+    auto a = const Array!(int)(1, 2, 3);
+    auto a2 = a;
+    auto a3 = a2.save();
+
+    assert(a2.front == 1);
+    static assert(!__traits(compiles, a2.front = 4));
+    static assert(!__traits(compiles, a2.popFront()));
+
+    auto a4 = a2.tail;
+    assert(a4.front == 2);
+    static assert(!__traits(compiles, a4 = a4.tail));
+}
+
+@safe unittest
+{
+    import std.conv;
+    testImmutability();
+    testConstness();
+    auto bytesUsed = _allocator.bytesUsed;
+    assert(bytesUsed == 0, "SList ref count leaks memory; leaked "
                            ~ to!string(bytesUsed) ~ " bytes");
 }
 
