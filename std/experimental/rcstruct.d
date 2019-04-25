350c40ee8c06d50b816eb3b884b0eb22ae2d318e
Eduard Staniloiu <edi33416@gmail.com>
2019-04-25 16:23:36 +0300

    Add function attributes

--- std/experimental/rcstruct.d
+++ std/experimental/rcstruct.d
@@ -89,6 +89,7 @@ else
 {
     alias pureAllocate = pureMalloc;
 
+    @nogc nothrow pure
     void pureDeallocate(T)(T[] b) { pureFree(b.ptr); }
 }
 
@@ -100,16 +101,19 @@ struct __mutable(T)
         size_t _ref = cast(size_t) null;
     }
 
-    this(T val) pure const nothrow
+    @nogc nothrow pure @trusted
+    this(T val) const
     {
         _ref = cast(size_t) val;
     }
 
-    this(shared T val) pure const nothrow
+    @nogc nothrow pure @trusted
+    this(shared T val) const
     {
         _ref = cast(size_t) val;
     }
 
+    @nogc nothrow pure @trusted
     T unwrap() const
     {
         return cast(T) _ref;
@@ -125,13 +129,19 @@ struct rcstruct
 
     private static enum isSharedMask = 1 << ((RC_T.sizeof * 8) - 1);
 
-    pure nothrow @nogc @safe scope
+    @nogc nothrow pure @safe scope
     bool isShared() const
     {
+        debug
+        {
+            import core.stdc.stdio;
+
+        }
+        auto x = rc.unwrap;
         return !!atomicOp!">="(*((() @trusted => cast(shared RC_T*) rc.unwrap)()), isSharedMask);
     }
 
-    pure nothrow @nogc @safe scope
+    @nogc nothrow pure @safe scope
     void setIsShared(bool _isShared) const
     {
         if (_isShared)
@@ -140,7 +150,7 @@ struct rcstruct
         }
     }
 
-    pure nothrow @nogc @trusted scope
+    @nogc nothrow pure @trusted scope
     RC_T rcOp(string op)(RC_T val) const
     {
         if (isShared())
@@ -153,7 +163,8 @@ struct rcstruct
         }
     }
 
-    this(this Q)(int) const pure scope
+    @nogc nothrow pure @trusted scope
+    this(this Q)(int) const
     {
         static if (is(Q == immutable))
         {
@@ -170,29 +181,35 @@ struct rcstruct
 
     private enum copyCtorIncRef = q{
         rc = rhs.rc;
-        if (rc.unwrap !is null)
+        assert(rc.unwrap == rhs.rc.unwrap);
+        if (rhs.rc.unwrap !is null)
         {
             setIsShared(rhs.isShared());
+            assert(isShared() == rhs.isShared());
             addRef();
         }
     };
 
+    @nogc nothrow pure @safe scope
     this(ref typeof(this) rhs)
     {
         mixin(copyCtorIncRef);
     }
 
     // { Get a const obj
+    @nogc nothrow pure @safe scope
     this(ref typeof(this) rhs) const
     {
         mixin(copyCtorIncRef);
     }
 
+    @nogc nothrow pure @safe scope
     this(const ref typeof(this) rhs) const
     {
         mixin(copyCtorIncRef);
     }
 
+    @nogc nothrow pure @safe scope
     this(immutable ref typeof(this) rhs) const
     {
         mixin(copyCtorIncRef);
@@ -200,20 +217,24 @@ struct rcstruct
     // } Get a const obj
 
     // { Get an immutable obj
+    @nogc nothrow pure @safe scope
     this(ref typeof(this) rhs) immutable
     {
         // Can't have an immutable ref to a mutable. Create a new RC
-        rc = cast(shared RC_T*) pureAllocate(RC_T.sizeof);
+        rc = (() @trusted => cast(shared RC_T*) pureAllocate(RC_T.sizeof))();
         setIsShared(true);
+        *rc.unwrap = 0;
+        addRef();
     }
 
-    @trusted
+    @nogc nothrow pure @safe scope
     this(const ref typeof(this) rhs) immutable
     {
-        if (isShared)
+        if (rhs.isShared)
         {
             // By implementation, only immutable RC is shared, so it's ok to inc ref
-            rc = cast(immutable) rhs.rc;
+            //rc = (() @trusted => cast(shared int *) rhs.rc.unwrap)();
+            rc = (() @trusted => cast(immutable) rhs.rc)();
             if (rc.unwrap !is null)
             {
                 setIsShared(rhs.isShared());
@@ -223,25 +244,30 @@ struct rcstruct
         else
         {
             // Can't have an immutable ref to a mutable. Create a new RC
-            rc = cast(shared RC_T*) pureAllocate(RC_T.sizeof);
+            rc = (() @trusted => cast(shared RC_T*) pureAllocate(RC_T.sizeof))();
             setIsShared(true);
+            *rc.unwrap = 0;
+            addRef();
         }
     }
 
+    @nogc nothrow pure @safe scope
     this(immutable ref typeof(this) rhs) immutable
     {
         mixin(copyCtorIncRef);
     }
     // } Get an immutable obj
 
-    private void* addRef() pure const @trusted scope
+    @nogc nothrow pure @safe scope
+    private void* addRef() const
     {
         assert(rc.unwrap !is null);
         rcOp!"+="(1);
         return null;
     }
 
-    private void* delRef() pure const @trusted scope
+    @nogc nothrow pure @safe scope
+    private void* delRef() const
     {
         assert(rc.unwrap !is null);
         RC_T counter = rcOp!"-="(1);
@@ -252,12 +278,14 @@ struct rcstruct
         return null;
     }
 
-    private void deallocate() pure const scope
+    @nogc nothrow pure @trusted scope
+    private void deallocate() const
     {
         pureDeallocate(rc.unwrap[0 .. 1]);
     }
 
-    ~this() pure const scope
+    @nogc nothrow pure @trusted scope
+    ~this() const
     {
         if (rc.unwrap !is null)
         {
@@ -271,7 +299,7 @@ struct rcstruct
         return rcOp!"=="(1) || rcOp!"=="(isSharedMask | 1);
     }
 
-    pure nothrow @nogc scope
+    pure nothrow @nogc @system scope
     auto getUnsafeValue() const
     {
         return rc.unwrap;
@@ -281,11 +309,23 @@ struct rcstruct
 version(CoreUnittest)
 unittest
 {
+    () @safe @nogc pure nothrow
     {
         rcstruct a = rcstruct(1);
+        assert(a.isUnique);
         const rcstruct ca = const rcstruct(1);
+        assert(ca.isUnique);
         immutable rcstruct ia = immutable rcstruct(1);
-    }
+        assert(ia.isUnique);
+
+        const c_cp_a = a;
+        const c_cp_ca = ca;
+        const c_cp_ia = ia;
+
+        immutable i_cp_a = a;
+        immutable i_cp_ca = ca;
+        immutable i_cp_ia = ia;
+    }();
 
     assert(allocator.bytesUsed == 0, "rcstruct leakes memory");
 }
