a4f3547a58c9a78ab1a82561c31815d6a5f9eeaa
Eduard Staniloiu <edi33416@gmail.com>
2019-05-15 18:05:35 +0300

    Refactor and use RefCount in simple array

--- std/experimental/rcstruct.d
+++ std/experimental/rcstruct.d
@@ -1,5 +1,3 @@
-import core.memory : pureMalloc, pureFree;
-
 private struct StatsAllocator
 {
     version(CoreUnittest) size_t bytesUsed;
@@ -85,43 +83,48 @@ version (CoreUnittest)
         allocator.deallocate(b);
     }
 }
-else
+
+struct __RefCount
 {
-    alias pureAllocate = pureMalloc;
-
-    @nogc nothrow pure
-    void pureDeallocate(T)(T[] b) { pureFree(b.ptr); }
-}
-
-struct __mutable(T)
-{
-    private union
+    private static struct __mutable(T)
     {
-        T _unused;
-        size_t _ref = cast(size_t) null;
+        private union
+        {
+            T _unused;
+            size_t _ref = cast(size_t) null;
+        }
+
+        @nogc nothrow pure @trusted
+        this(T val) const
+        {
+            _ref = cast(size_t) val;
+        }
+
+        @nogc nothrow pure @trusted
+        this(shared T val) const
+        {
+            _ref = cast(size_t) val;
+        }
+
+        @nogc nothrow pure @trusted
+        T unwrap() const
+        {
+            return cast(T) _ref;
+        }
     }
 
-    @nogc nothrow pure @trusted
-    this(T val) const
+    version(CoreUnittest) {} else
     {
-        _ref = cast(size_t) val;
-    }
+        import core.memory : pureMalloc, pureFree;
 
-    @nogc nothrow pure @trusted
-    this(shared T val) const
-    {
-        _ref = cast(size_t) val;
-    }
+        private alias pureAllocate = pureMalloc;
 
-    @nogc nothrow pure @trusted
-    T unwrap() const
-    {
-        return cast(T) _ref;
+        @nogc nothrow pure
+        private static void pureDeallocate(T)(T[] b)
+        {
+            pureFree(b.ptr);
+        }
     }
-}
-
-struct RefCount
-{
     import core.atomic : atomicOp;
 
     alias CounterType = uint;
@@ -130,7 +133,8 @@ struct RefCount
     @nogc nothrow pure @safe scope
     bool isShared() const
     {
-        return (cast(size_t) rc.unwrap) % 8 == 0;
+        // Faster than ((cast(size_t) rc.unwrap) % 8) == 0;
+        return !((cast(size_t) rc.unwrap) & 7);
     }
 
     @nogc nothrow pure @trusted scope
@@ -165,7 +169,7 @@ struct RefCount
     private enum copyCtorIncRef = q{
         rc = rhs.rc;
         assert(rc.unwrap == rhs.rc.unwrap);
-        if (rhs.rc.unwrap !is null)
+        if (rhs.isInitialized())
         {
             assert(isShared() == rhs.isShared());
             addRef();
@@ -211,12 +215,12 @@ struct RefCount
     @nogc nothrow pure @safe scope
     this(const ref typeof(this) rhs) immutable
     {
-        if (rhs.isShared)
+        if (rhs.isShared())
         {
             // By implementation, only immutable RC is shared, so it's ok to inc ref
             //rc = (() @trusted => cast(shared int *) rhs.rc.unwrap)();
             rc = (() @trusted => cast(immutable) rhs.rc)();
-            if (rc.unwrap !is null)
+            if (isInitialized())
             {
                 addRef();
             }
@@ -238,9 +242,28 @@ struct RefCount
     // } Get an immutable obj
 
     @nogc nothrow pure @safe scope
+    ref __RefCount opAssign(ref typeof(this) rhs)
+    {
+        if (rhs.isInitialized() && rc.unwrap == rhs.rc.unwrap)
+        {
+            return this;
+        }
+        if (rhs.isInitialized())
+        {
+            rhs.addRef();
+        }
+        if (isInitialized())
+        {
+            delRef();
+        }
+        rc = rhs.rc;
+        return this;
+    }
+
+    @nogc nothrow pure @safe scope
     private void* addRef() const
     {
-        assert(rc.unwrap !is null);
+        assert(isInitialized());
         rcOp!"+="(1);
         return null;
     }
@@ -248,9 +271,8 @@ struct RefCount
     @nogc nothrow pure @trusted scope
     private void* delRef() const
     {
-        assert(rc.unwrap !is null);
-        CounterType counter = rcOp!"-="(1);
-        if (counter == 0)
+        assert(isInitialized());
+        if (rcOp!"=="(1) || (rcOp!"-="(1) == 0))
         {
             deallocate();
         }
@@ -271,9 +293,9 @@ struct RefCount
     }
 
     @nogc nothrow pure @trusted scope
-    ~this() const
+    ~this()
     {
-        if (rc.unwrap !is null)
+        if (isInitialized())
         {
             delRef();
         }
@@ -282,10 +304,17 @@ struct RefCount
     pure nothrow @safe @nogc scope
     bool isUnique() const
     {
-        return (rc.unwrap is null) || rcOp!"=="(1);
+        assert(isInitialized(), "[__RefCount.isUnique] __RefCount is uninitialized");
+        return !!rcOp!"=="(1);
     }
 
-    pure nothrow @nogc @system scope
+    pure nothrow @safe @nogc scope
+    bool isInitialized() const
+    {
+        return rc.unwrap !is null;
+    }
+
+    pure nothrow @nogc @system
     CounterType* getUnsafeValue() const
     {
         return rc.unwrap;
@@ -297,11 +326,11 @@ unittest
 {
     () @safe @nogc pure nothrow
     {
-        RefCount a = RefCount(1);
+        __RefCount a = __RefCount(1);
         assert(a.isUnique);
-        const RefCount ca = const RefCount(1);
+        const __RefCount ca = const __RefCount(1);
         assert(ca.isUnique);
-        immutable RefCount ia = immutable RefCount(1);
+        immutable __RefCount ia = immutable __RefCount(1);
         assert(ia.isUnique);
 
         // A const reference will increase the ref count
@@ -330,12 +359,203 @@ unittest
         assert((() @trusted => *cast(int*)i_cp_c_cp_ia.getUnsafeValue() == 4)());
         assert((() @trusted => i_cp_c_cp_ia.getUnsafeValue() == c_cp_ia.getUnsafeValue())());
 
-        RefCount t;
-        assert(t.isUnique());
-        RefCount t2 = t;
-        assert(t.isUnique());
-        assert(t2.isUnique());
+        __RefCount t;
+        assert(!t.isInitialized());
+        __RefCount t2 = t;
+        assert(!t.isInitialized());
+        assert(!t2.isInitialized());
     }();
 
-    assert(allocator.bytesUsed == 0, "RefCount leakes memory");
+    assert(allocator.bytesUsed == 0, "__RefCount leakes memory");
+}
+
+version(CoreUnittest)
+unittest
+{
+    () @safe @nogc pure nothrow
+    {
+        __RefCount a = __RefCount(1);
+        assert(a.isUnique);
+        __RefCount a2 = a;
+        assert((() @trusted => *cast(int*)a.getUnsafeValue() == 2)());
+        __RefCount a3 = __RefCount(1);
+        a2 = a3;
+        assert((() @trusted => *cast(int*)a.getUnsafeValue() == 1)());
+        assert(a.isUnique);
+    }();
+
+    assert(allocator.bytesUsed == 0, "__RefCount leakes memory");
+}
+
+version(CoreUnittest)
+unittest
+{
+    struct TestRC
+    {
+        private __RefCount rc;
+        int[] payload;
+
+        @nogc nothrow pure @trusted scope
+        this(this Q)(int sz) const
+        {
+            rc = __RefCount(1);
+            static if (is(Q == immutable))
+            {
+                payload = (cast(immutable int*) pureAllocate(sz * int.sizeof))[0 .. sz];
+            }
+            else
+            {
+                payload = (cast(int*) pureAllocate(sz * int.sizeof))[0 .. sz];
+            }
+        }
+
+        private enum copyCtorIncRef = q{
+            rc = rhs.rc;
+            payload = rhs.payload;
+        };
+
+        @nogc nothrow pure @safe scope
+        this(ref typeof(this) rhs)
+        {
+            mixin(copyCtorIncRef);
+        }
+
+        // { Get a const obj
+        @nogc nothrow pure @safe scope
+        this(ref typeof(this) rhs) const
+        {
+            mixin(copyCtorIncRef);
+        }
+
+        @nogc nothrow pure @safe scope
+        this(const ref typeof(this) rhs) const
+        {
+            mixin(copyCtorIncRef);
+        }
+
+        @nogc nothrow pure @safe scope
+        this(immutable ref typeof(this) rhs) const
+        {
+            mixin(copyCtorIncRef);
+        }
+        // } Get a const obj
+
+        // { Get an immutable obj
+        @nogc nothrow pure @trusted scope
+        this(ref typeof(this) rhs) immutable
+        {
+            // Can't have an immutable ref to a mutable. Create a new RC
+            rc = rhs.rc;
+            auto sz = rhs.payload.length;
+            int[] tmp = (cast(int*) pureAllocate(sz * int.sizeof))[0 .. sz];
+            tmp[] = rhs.payload[];
+            payload = cast(immutable) tmp;
+        }
+
+        @nogc nothrow pure @safe scope
+        this(const ref typeof(this) rhs) immutable
+        {
+            rc = rhs.rc;
+            if (rhs.rc.isShared)
+            {
+                // By implementation, only immutable RC is shared, so it's ok to inc ref
+                payload = (() @trusted => cast(immutable) rhs.payload)();
+            }
+            else
+            {
+                // Can't have an immutable ref to a mutable. Create a new RC
+                auto sz = rhs.payload.length;
+                int[] tmp = (() @trusted => (cast(int*) pureAllocate(sz * int.sizeof))[0 .. sz])();
+                tmp[] = rhs.payload[];
+                payload = (() @trusted => cast(immutable) tmp)();
+            }
+        }
+
+        @nogc nothrow pure @safe scope
+        this(immutable ref typeof(this) rhs) immutable
+        {
+            mixin(copyCtorIncRef);
+        }
+        // } Get an immutable obj
+
+        @nogc nothrow pure @safe scope
+        ref TestRC opAssign(ref typeof(this) rhs)
+        {
+            if (payload is rhs.payload)
+            {
+                return this;
+            }
+            if (rc.isInitialized && rc.isUnique)
+            {
+                () @trusted { pureDeallocate(payload); }();
+            }
+            payload = rhs.payload;
+            rc = rhs.rc;
+            return this;
+        }
+
+        @nogc nothrow pure @trusted scope
+        ~this()
+        {
+            if (rc.isInitialized() && rc.isUnique())
+            {
+                pureDeallocate(cast(int[]) payload);
+            }
+        }
+    }
+
+    () @safe @nogc pure nothrow
+    {
+        enum numElem = 10;
+        auto t = TestRC(numElem);
+        assert(t.rc.isUnique);
+        const TestRC ct = const TestRC(numElem);
+        assert(ct.rc.isUnique);
+        immutable TestRC it = immutable TestRC(numElem);
+        assert(it.rc.isUnique);
+
+        // A const reference will increase the ref count
+        const c_cp_t = t;
+        assert((() @trusted => *cast(int*)t.rc.getUnsafeValue() == 2)());
+        assert(t.payload is c_cp_t.payload);
+        const c_cp_ct = ct;
+        assert((() @trusted => *cast(int*)ct.rc.getUnsafeValue() == 2)());
+        assert(ct.payload is c_cp_ct.payload);
+        const c_cp_it = it;
+        assert((() @trusted => *cast(int*)it.rc.getUnsafeValue() == 2)());
+        assert(it.payload is c_cp_it.payload);
+
+        // An immutable from a mutable reference will create a copy
+        immutable i_cp_t = t;
+        assert((() @trusted => *cast(int*)t.rc.getUnsafeValue() == 2)());
+        assert((() @trusted => *cast(int*)i_cp_t.rc.getUnsafeValue() == 1)());
+        assert(t.payload !is i_cp_t.payload);
+        // An immutable from a const to a mutable reference will create a copy
+        immutable i_cp_ct = ct;
+        assert((() @trusted => *cast(int*)ct.rc.getUnsafeValue() == 2)());
+        assert((() @trusted => *cast(int*)i_cp_ct.rc.getUnsafeValue() == 1)());
+        assert(ct.payload !is i_cp_ct.payload);
+        // An immutable from an immutable reference will increase the ref count
+        immutable i_cp_it = it;
+        assert((() @trusted => *cast(int*)it.rc.getUnsafeValue() == 3)());
+        assert((() @trusted => *cast(int*)i_cp_it.rc.getUnsafeValue() == 3)());
+        assert(it.payload is i_cp_it.payload);
+        // An immutable from a const to an immutable reference will increase the ref count
+        immutable i_cp_c_cp_it = c_cp_it;
+        assert((() @trusted => *cast(int*)c_cp_it.rc.getUnsafeValue() == 4)());
+        assert((() @trusted => *cast(int*)i_cp_c_cp_it.rc.getUnsafeValue() == 4)());
+        assert((() @trusted => i_cp_c_cp_it.rc.getUnsafeValue() == c_cp_it.rc.getUnsafeValue())());
+        assert(c_cp_it.payload is i_cp_c_cp_it.payload);
+
+        // Ensure uninitialized structs don't crash
+        TestRC t1;
+        assert(!t1.rc.isInitialized);
+        TestRC t2 = t1;
+        assert(!t1.rc.isInitialized);
+        assert(!t2.rc.isInitialized);
+        TestRC t3 = TestRC(numElem);
+        t2 = t3;
+    }();
+
+    assert(allocator.bytesUsed == 0, "__RefCount leakes memory");
 }
